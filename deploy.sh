b24a4fef048c1bf3ca79b7e8ea2e9a631216c03c
Enoch Tam <me@enocht.am>
2016-11-27 15:11:58 -0500

    Moved deploy script to serve

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,4 @@
-#!/usr/bin/expect -f
+#!/bin/bash
 
-spawn ssh enoch@qlicker.etdev.ca
-expect "password:"
-sleep 1
-send "$METEOR_DEPLOY_PASS\r"
-
-cd ~/build/qlicker
-git pull
-npm install
-rm qlicker.tar.gz
-meteor build .
-rm /home/qlicker/qlicker.tar.gz
-mv qlicker.tar.gz /home/qlicker/qlicker.tar.gz
-rm -rf /home/qlicker/bundle
-cd /home/qlicker/
-tar -zxf /home/qlicker/qlicker.tar.gz
-(cd bundle/programs/server && npm install)
-sudo /sbin/restart qlicker
+ssh enoch@qlicker.etdev.ca /home/enoch/deploy.sh
 
