08b780885c96a488113f9c2c200eed0cfd1a5c90
Enoch Tam <me@enocht.am>
2017-03-03 17:07:18 -0500

    Reversed changes to QuestionDisplay.jsx

--- imports/ui/QuestionDisplay.jsx
+++ imports/ui/QuestionDisplay.jsx
@@ -13,27 +13,8 @@ export class QuestionDisplay extends Component {
   constructor (p) {
     super(p)
 
-    this.submitAnswer = this.submitAnswer.bind(this)
-
     this.readonly = false
     if (this.props.readonly) this.readonly = this.props.readonly
-  }
-
-  submitAnswer (answer) {
-    const answerObject = {
-      studentUserId: Meteor.userId(),
-      answer: answer,
-      attempt: this.props.attempt.number,
-      questionId: this.props.question._id
-    }
-    Meteor.call('answer.addQuestionAnswer', answerObject, (err, answerId) => {
-      if (err) {
-        alertify.error('Error: ' + err.error)
-      } else {
-        alertify.success('Answer Submitted')
-        // success. do stuff here if needed
-      }
-    })
   }
 
   componentDidMount () {
@@ -54,15 +35,15 @@ export class QuestionDisplay extends Component {
 
       <div className='ql-answers'>
         {
-          q.options.map((a) => {
+          q.answers.map((a) => {
             let content
 
             if (a.wysiwyg) {
-              content = (<div onClick={() => this.submitAnswer(a.answer)} className='ql-wysiwyg-content' key={'answer_' + a.answer}>
+              content = (<div className='ql-wysiwyg-content' key={'answer_' + a.answer}>
                 { WysiwygHelper.htmlDiv(a.content) }
               </div>)
             } else {
-              content = (<div onClick={() => this.submitAnswer(a.answer)} className='ql-tf-content' key={'answer_' + a.answer}>
+              content = (<div className='ql-tf-content' key={'answer_' + a.answer}>
                 <span className={a.answer === 'TRUE' ? 'correct-color' : 'incorrect-color'}>{a.answer}</span>
               </div>)
             }
