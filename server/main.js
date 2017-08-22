ef39cf0908db7471ee4fd9c5ee16247b40a965ee
Turner <tugarrow15@gmail.com>
2017-08-22 13:11:09 -0400

    Removed setting MAIL_URL

--- server/main.js
+++ server/main.js
@@ -18,8 +18,4 @@ Meteor.startup(() => {
       email: ''
     })
   }
-  if (process.env.QLICKER_MAIL_PASSWORD) {
-    process.env.MAIL_URL = 'smtp://admin%40qlicker.etdev.ca:' + process.env.QLICKER_MAIL_PASSWORD + '@smtp.mailgun.org:587'
-
-  }
 })
