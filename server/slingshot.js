8d9fd895a9b511c918883d8691cf44b3b60975ff
Viraj Bangari <Viraj3f@gmail.com>
2018-01-16 20:45:43 -0500

    Create fake storage service

--- server/slingshot.js
+++ server/slingshot.js
@@ -3,21 +3,47 @@ import { Slingshot } from 'meteor/edgee:slingshot'
 
 import { Settings } from '../imports/api/settings.js'
 
-Slingshot.createDirective('QuestionImages', Slingshot.S3Storage, {
-  allowedFileTypes: ['image/png', 'image/jpeg', 'image/jpg', 'image/gif'],
-  maxSize: null, // Unlimited, handled in authorized() instead
-  bucket: Meteor.settings.bucket,
-  acl: 'public-read',
+let hasS3Credentials = Meteor.settings.AWSAccessKeyId &&
+    Meteor.settings.AWSSecretAccessKey &&
+    Meteor.settings.bucket
 
-  authorize: function (file, metaContext) {
-    if (file.size > (Settings.findOne().maxImageSize * 1024 * 1024)) {
-      alertify.error('Image too large')
-      return false
+if (hasS3Credentials) {
+  Slingshot.createDirective('QuestionImages', Slingshot.S3Storage, {
+    allowedFileTypes: ['image/png', 'image/jpeg', 'image/jpg', 'image/gif'],
+    maxSize: null, // Unlimited, handled in authorized() instead
+    bucket: Meteor.settings.bucket,
+    acl: 'public-read',
+
+    authorize: function (file, metaContext) {
+      if (file.size > (Settings.findOne().maxImageSize * 1024 * 1024)) {
+        alertify.error('Image too large')
+        return false
+      }
+      return Meteor.userId()
+    },
+
+    key: function (file, metaContext) {
+      return metaContext.UID + '/' + metaContext.type
     }
-    return Meteor.userId()
-  },
-
-  key: function (file, metaContext) {
-    return metaContext.UID + '/' + metaContext.type
+  })
+} else {
+  let fakeStorageService = {
+    directiveMatch: {},
+    directiveDefault: {
+      authorize: function (file, metaContext) {
+        throw new Meteor.Error('no-s3-credentials', 'Missing S3 credentials. Are you in a dev environment?')
+      },
+      maxSize: null,
+      allowedFileTypes: null
+    },
+    upload: function (method, directive, file, meta) {}
   }
-})
+
+  Slingshot.createDirective('QuestionImages', fakeStorageService, {})
+
+  console.log(
+    'WARNING: ' +
+    'Missing S3 credentials in meteor settings. Image uploading' +
+    ' will not work. This is fine if you are doing some sort of minor' +
+    ' testing. Do NOT deploy if you are seeing this message in prod!')
+}
