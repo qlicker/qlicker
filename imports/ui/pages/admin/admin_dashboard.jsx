bd3e3dd8a1f7981a3fa618ae37dd2e5bbe14369b
PfeifferH <15hp19@queensu.ca>
2018-05-25 16:40:52 -0400

    Grouped admin_dashboard settings

--- imports/ui/pages/admin/admin_dashboard.jsx
+++ imports/ui/pages/admin/admin_dashboard.jsx
@@ -141,122 +141,124 @@ class _AdminDashboard extends Component {
     
     return (
       <div className='container ql-admin-page'>
-        <h2>Admin User Management</h2>
+        <h1>Dashboard</h1>
         <br />
 
-        <h4>Maximum image size (in MB, after rescaling to max width)</h4>
-        <form ref='imageSizeForm' onSubmit={this.saveImageSize} className='form-inline'>
-          <input className='form-control' value={this.state.size} type='text' onChange={setImageSize} placeholder='Image size' />
-          <input type='submit' className='btn btn-primary' value='Set' />
-        </form>
+        <div>
+          <h4>Support email</h4>
+          <form ref='supportForm' onSubmit={this.saveEmail} className='form-inline'>
+            <input className='form-control' value={this.state.supportEmail} type='text' onChange={setSupportEmail} placeholder='Support email' />
+            <input type='submit' className='btn btn-primary' value='Set' />
+          </form>
+        </div>
+        
 
-        <h4>Maximum image width (px)</h4>
-        <form ref='imageWidthForm' onSubmit={this.saveImageWidth} className='form-inline'>
-          <input className='form-control' value={this.state.width} type='text' onChange={setImageWidth} placeholder='Image width' />
-          <input type='submit' className='btn btn-primary' value='Set' />
-        </form>
-
-        <h4>Support email</h4>
-        <form ref='supportForm' onSubmit={this.saveEmail} className='form-inline'>
-          <input className='form-control' value={this.state.supportEmail} type='text' onChange={setSupportEmail} placeholder='Support email' />
-          <input type='submit' className='btn btn-primary' value='Set' />
-        </form>
-
-        <h4>Require verified email to login</h4>
-        <input type='checkbox' checked={this.state.requireVerified} onChange={this.setVerified} />
-
-        <h4>AWS Bucket Name</h4>
-        <form ref='supportForm' onSubmit={this.saveEmail} className='form-inline'>
-          <input className='form-control' value={this.state.bucket} type='text' onChange={setBucket} />
-          <input type='submit' className='btn btn-primary' value='Set' />
-        </form>
-
-        <h4>AWS Bucket Region</h4>
-        <form ref='supportForm' onSubmit={this.saveEmail} className='form-inline'>
-          <input className='form-control' value={this.state.region} type='text' onChange={setRegion} />
-          <input type='submit' className='btn btn-primary' value='Set' />
-        </form>
-
-        <h4>AWS Access Key Id</h4>
-        <form ref='supportForm' onSubmit={this.saveEmail} className='form-inline'>
-          <input className='form-control' value={this.state.accessKey} type='text' onChange={setAccessKey} />
-          <input type='submit' className='btn btn-primary' value='Set' />
-        </form>
-
-        <h4>AWS Secret</h4>
-        <form ref='supportForm' onSubmit={this.saveEmail} className='form-inline'>
-          <input className='form-control' value={this.state.secret} type='text' onChange={setSecret} />
-          <input type='submit' className='btn btn-primary' value='Set' />
-        </form>
-
-        <input className='btn btn-primary' value='Set' onClick={this.saveCredentials} />
-
-        <RestrictDomainForm
-          done={() => { return true }}
-          settings={this.props.settings}
-        />
-
+        <h2>Image Settings</h2>
         <br />
-        <h4>Users (with elevated permissions first)</h4>
-        <table className='table table-bordered'>
-          <tbody>
-            <tr>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Courses</th>
-              <th>Change Role</th>
-            </tr>
+        <div className='ql-admin-settings'>
+          <div> 
+            <h4>Maximum image size (in MB, after rescaling to max width)</h4>
+            <form ref='imageSizeForm' onSubmit={this.saveImageSize} className='form-inline'>
+              <input className='form-control' value={this.state.size} type='text' onChange={setImageSize} placeholder='Image size' />
+              <input type='submit' className='btn btn-primary' value='Set' />
+            </form>
 
-            {
-              this.props.allUsers.map((u) => {
-                let courseList = ''
-                if (u.profile.courses && this.props) {
-                  u.profile.courses.forEach(function (cId) {
-                    courseList += this.props.courseNames[cId] ? this.props.courseNames[cId] + ' ' : ''
-                  }.bind(this))
-                }
-                return (<tr key={u._id}>
-                  <td>
-                    <a href='#' onClick={(e) => this.toggleProfileViewModal(u)}>{u.getName()}</a>
-                  </td>
-                  <td>{u.getEmail()} &nbsp;&nbsp; {u.emails[0].verified ? '(verified)'
-                      : <a href='#' onClick={(e) => this.verifyUserEmail(u.getEmail())}>Verify</a>}
-                  </td>
-                  <td>{courseList}</td>
-                  <td>
-                    <select onChange={(e) => this.saveRoleChange(u._id, e.target.value)} value={u.getRole()}>
-                      { Object.keys(ROLES).map((r) => <option key={'role_' + ROLES[r]} value={ROLES[r]}>{ROLES[r]}</option>)}
-                    </select>
-                    &nbsp;&nbsp;{u.isInstructorAnyCourse() && u.hasRole('student') ? '(TA)' : ''}
-                  </td>
-                </tr>)
-              })
-            }
-          </tbody>
-        </table>
-
-        <h4>Add user manually</h4>
-        <form className='ql-admin-login-box col-md-4' onSubmit={this.handleSubmit}>
-          <div className='ql-card-content inputs-container'>
-            <div className='input-group'>
-              <input className='form-control' type='text' data-name='firstname' onChange={this.setValue} placeholder='First Name' />
-              <input className='form-control' type='text' data-name='lastname' onChange={this.setValue} placeholder='Last Name' />
+            <h4>Maximum image width (px)</h4>
+            <form ref='imageWidthForm' onSubmit={this.saveImageWidth} className='form-inline'>
+              <input className='form-control' value={this.state.width} type='text' onChange={setImageWidth} placeholder='Image width' />
+              <input type='submit' className='btn btn-primary' value='Set' />
+            </form>
+          </div>   
+            
+          <form className='ql-admin-login-box col-md-4' onSubmit={this.saveCredentials}>
+            <h4>Change AWS Credentials</h4> 
+            <div className='ql-card-content inputs-container'>
+              <div className='input-group'>
+                <input className='form-control' type='text' value={this.state.bucket} onChange={setBucket} placeholder='Bucket Name' />
+                <input className='form-control' type='text' value={this.state.region} onChange={setRegion} placeholder='Region' />
+              </div>         
+              <input className='form-control' type='text' value={this.state.accessKey} onChange={setAccessKey} placeholder='AWS Access Key Id' /><br />
+              <input className='form-control' type='text' value={this.state.secret} onChange={setSecret} placeholder='AWS Secret' /><br />
+              <div className='spacer1'>&nbsp;</div>
+              <input type='submit' id='submitButton' className='btn btn-primary btn-block' value='Submit' />
             </div>
+          </form>        
+        </div>
 
-            <input className='form-control' id='emailField' type='email' data-name='email' onChange={this.setValue} placeholder='Email' /><br />
+        <h2>User Settings</h2>
+        <br />
+        <div className='ql-admin-settings'>
+          <div>
+            <h4>Require verified email to login</h4>
+            <input type='checkbox' checked={this.state.requireVerified} onChange={this.setVerified} />
 
-            <input className='form-control' id='passwordField' type='password' data-name='password' onChange={this.setValue} placeholder='Password' /><br />
-            <div><input className='form-control' type='password' data-name='password_verify' onChange={this.setValue} placeholder='Retype Password' /><br /></div>
-
-            <div className='spacer1'>&nbsp;</div>
-            <input type='submit' id='submitButton' className='btn btn-primary btn-block' value='Submit' />
+            <RestrictDomainForm
+              done={() => { return true }}
+              settings={this.props.settings}
+            />
           </div>
-        </form>
-        { this.state.profileViewModal
-          ? <ProfileViewModal
-            user={this.state.userToView}
-            done={this.toggleProfileViewModal} />
-        : '' }
+          
+          <form className='ql-admin-login-box col-md-4' onSubmit={this.handleSubmit}>
+            <h4>Add user manually</h4>  
+            <div className='ql-card-content inputs-container'>
+              <div className='input-group'>
+                <input className='form-control' type='text' data-name='firstname' onChange={this.setValue} placeholder='First Name' />
+                <input className='form-control' type='text' data-name='lastname' onChange={this.setValue} placeholder='Last Name' />
+              </div>
+              <input className='form-control' id='emailField' type='email' data-name='email' onChange={this.setValue} placeholder='Email' />
+              <br />
+              <input className='form-control' id='passwordField' type='password' data-name='password' onChange={this.setValue} placeholder='Password' /><br />
+              <div><input className='form-control' type='password' data-name='password_verify' onChange={this.setValue} placeholder='Retype Password' /><br /></div>
+              <div className='spacer1'>&nbsp;</div>
+              <input type='submit' id='submitButton' className='btn btn-primary btn-block' value='Submit' />
+            </div>
+          </form>
+        </div>
+
+        <div>
+          <h1>Users (with elevated permissions first)</h1>
+          <table className='table table-bordered'>
+            <tbody>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Courses</th>
+                <th>Change Role</th>
+              </tr>
+
+              {
+                this.props.allUsers.map((u) => {
+                  let courseList = ''
+                  if (u.profile.courses && this.props) {
+                    u.profile.courses.forEach(function (cId) {
+                      courseList += this.props.courseNames[cId] ? this.props.courseNames[cId] + ' ' : ''
+                    }.bind(this))
+                  }
+                  return (<tr key={u._id}>
+                    <td>
+                      <a href='#' onClick={(e) => this.toggleProfileViewModal(u)}>{u.getName()}</a>
+                    </td>
+                    <td>{u.getEmail()} &nbsp;&nbsp; {u.emails[0].verified ? '(verified)'
+                        : <a href='#' onClick={(e) => this.verifyUserEmail(u.getEmail())}>Verify</a>}
+                    </td>
+                    <td>{courseList}</td>
+                    <td>
+                      <select onChange={(e) => this.saveRoleChange(u._id, e.target.value)} value={u.getRole()}>
+                        { Object.keys(ROLES).map((r) => <option key={'role_' + ROLES[r]} value={ROLES[r]}>{ROLES[r]}</option>)}
+                      </select>
+                      &nbsp;&nbsp;{u.isInstructorAnyCourse() && u.hasRole('student') ? '(TA)' : ''}
+                    </td>
+                  </tr>)
+                })
+              }
+            </tbody>
+          </table>
+          { this.state.profileViewModal
+            ? <ProfileViewModal
+              user={this.state.userToView}
+              done={this.toggleProfileViewModal} />
+          : '' }
+        </div>
       </div>)
   }
 }
