4efc0e6ccd448c55d94e9674fd2b7d9d5a02434b
Ryan Martin <ryanmartinneutrino@gmail.com>
2018-08-15 09:27:13 -0400

    full functionning SSO, yay

--- server/saml_server.js
+++ server/saml_server.js
@@ -13,20 +13,8 @@ console.log(Meteor.absoluteUrl())
 
 settings = Settings.findOne({})
 
-/* Testing */
-//console.log(settings)
-SAMLRequest = Assets.getText('SAMLRequest')
-var xml = new Buffer(SAMLRequest, 'base64').toString('utf8');
-var dom = new xmldom.DOMParser().parseFromString(xml); 
-var sessionIndex = xpath(dom, "/*[local-name()='LogoutRequest']/*[local-name()='SessionIndex']/text()")[0].data;
-
-
-
-
-
-/* End testing */
-
-//Only if SSO is enabled, set things up:
+//Only if SSO is enabled, set things up
+//These setting take effect only after restarting the app, if the SSO is enabled/disabled
 if(settings.SSO_enabled && settings.SSO_emailIdentifier && settings.SSO_entrypoint && settings.SSO_identifierFormat ){
     
 //Create a passport-saml strategy using the given settings
@@ -35,15 +23,15 @@ if(settings.SSO_enabled && settings.SSO_emailIdentifier && settings.SSO_entrypoi
     
   strategy = new saml.Strategy({
     callbackUrl: Meteor.absoluteUrl('SSO/SAML2'),
-    logoutCallbackUrl: Meteor.absoluteUrl('logout'),
+    logoutCallbackUrl: Meteor.absoluteUrl('logout'), //TODO: update this to SSO/SAML2/logout
     entryPoint: settings.SSO_entrypoint,
     cert: settings.SSO_cert,
     identifierFormat: settings.SSO_identifierFormat,
-    logoutUrl: (settings.SSO_logoutUrl ? settings.SSO_logoutUrl : settings.SSO_entrypoint),
+    logoutUrl: (settings.SSO_logoutUrl ? settings.SSO_logoutUrl : ''),
       
     //privateCert: Assets.getText('key.key'),
     decryptionPvk: Assets.getText('key.key'),
-    issuer: 'passport-saml' // call this Qlicker, not passport-saml!
+    issuer: 'passport-saml' // TODO: call this Qlicker, not passport-saml!
     },
     function(profile, done) {
     return done(null, profile);
@@ -59,29 +47,31 @@ if(settings.SSO_enabled && settings.SSO_emailIdentifier && settings.SSO_entrypoi
   //LoginResultForCredentialToken stores {token: {profile, nameID, sessionIndex}}
   //The token is created by the client, passed as RelayState to the IDP, and then 
   //accessed again on the server; this is how the server know the request from the IDP
-  //goes with a particular client
+  //goes with a particular client login attempt
   Accounts.saml._loginResultForCredentialToken = {};
 
-  // Inserted during IdP -> SP Callback
+  //Inserted during IdP -> SP Callback
   Accounts.saml.insertCredential = function (credentialToken, samlInfo) {
     Accounts.saml._loginResultForCredentialToken[credentialToken] = samlInfo;
   }
 
-  // Retrieved in account login handler
+  //Retrieved in account login handler
   Accounts.saml.retrieveCredential = function(credentialToken) {
     let profile = Accounts.saml._loginResultForCredentialToken[credentialToken];
     delete Accounts.saml._loginResultForCredentialToken[credentialToken];
     return profile;
   };
 
-  //Register a login handler with Meteor (gets called by client, in loginWithSaml
+  //Register a login handler with Meteor (gets called by client, in loginWithSaml through
+  //Accounts.callLoginMethod)
   Accounts.registerLoginHandler(function (loginRequest) {
+    //Check if the login request is consistent with an SSO request
     if (loginRequest.credentialToken && loginRequest.saml) {
       const samlInfo = Accounts.saml.retrieveCredential(loginRequest.credentialToken);     
       if (samlInfo) {
+        //Try to find a user based on the SAML email addresss, otherwise, create a new user  
         const samlProfile = samlInfo.profile
         let userId = null
-          //let user = Meteor.users.findOne({ "emails.address" : samlProfile.email })
         let user = Accounts.findUserByEmail(samlProfile.email)
         let profile = (user && user.profile) ? user.profile : samlProfile
         let services = { sso: {id: samlInfo.nameID, nameIDFormat: samlInfo.nameIDFormat } }
@@ -91,6 +81,7 @@ if(settings.SSO_enabled && settings.SSO_emailIdentifier && settings.SSO_entrypoi
           for (key in samlProfile){
             profile[key] = samlProfile[key]   
           }  
+          //Note that it will not actually update the email address, since the user was found by email address  
           Meteor.users.update(userId, {$set: {email: profile.email,
                                               'emails.0.verified': true,
                                               profile: profile,
@@ -111,10 +102,9 @@ if(settings.SSO_enabled && settings.SSO_emailIdentifier && settings.SSO_entrypoi
         let hashStampedToken = Accounts._hashStampedToken(stampedToken)
         Meteor.users.update(userId, { $push: { 'services.resume.loginTokens': hashStampedToken},
                                       $set: { 'services.sso.session': {sessionIndex: samlInfo.sessionIndex,
-                                                                         loginToken: hashStampedToken.hashedToken }}})
-   
-        user = Meteor.users.findOne(userId)
-        console.log(user)
+                                                                       loginToken: hashStampedToken.hashedToken }}})   
+        //user = Meteor.users.findOne(userId)
+        //console.log(user)
         return {
           userId: userId,
           token: stampedToken.token
@@ -125,9 +115,12 @@ if(settings.SSO_enabled && settings.SSO_emailIdentifier && settings.SSO_entrypoi
     }
   })
 
-
+  //This method provides a link to the SSO logout with the required SAML request.
+  //This is the link that goes with the Logout From SSO button in page_container
+  //This is based on what was done in https://github.com/lucidprogrammer/meteor-saml-sp/blob/master/src/server/samlServerHandler.js
   Meteor.methods({
    "getSSOLogoutUrl": (callback) => {
+      if (settings.SSO_logoutUrl === '') return null
       user = Meteor.user()
       if (!user || !user.services || !user.services.sso || !user.services.sso.session || !user.services.sso.session.sessionIndex) return null
       var getLogoutLinkSync =  Meteor.wrapAsync(getSSLogoutAsync);
@@ -140,6 +133,7 @@ if(settings.SSO_enabled && settings.SSO_emailIdentifier && settings.SSO_entrypoi
       let request  = {user : {nameID : user.services.sso.id , nameIDFormat: user.services.sso.nameIDFormat, sessionIndex: user.services.sso.session.sessionIndex}  };
       let getLogout = Accounts.samlStrategy._saml.getLogoutUrl(request, function(error,url){
          if(error) console.log(error);
+         //The IDP POST request results in the logout and erasing the session, so no need to do it here
          //console.log("url");
          //console.log(url);
          /*Fiber(function () {
@@ -149,26 +143,30 @@ if(settings.SSO_enabled && settings.SSO_emailIdentifier && settings.SSO_entrypoi
        })
   }
 
+  //Add server side routes to handle the SSO stuff
   WebApp.connectHandlers
     .use(bodyParser.urlencoded({ extended: false }))
-    .use('/logout', function(req, res, next) {     
+    .use('/logout', function(req, res, next) {
+      //TODO: Move this code to SSO/SAML2/logout route and update the corresponding logout URL in the settings!!!!
       Fiber(function() {
         if (req.method === 'POST') {  
+          //----------- Hack start
           //A hack to bypass the SSO stuff and log the user out from the IDP POST request
           //(needed because passport-saml cannot validate the encrypted response)
           //WARNING: This does not do any safety checks on the request passed by the IDP!!!
           let xml = new Buffer(req.body.SAMLRequest, 'base64').toString('utf8');
           let dom = new xmldom.DOMParser().parseFromString(xml); 
           let sessionIndex = xpath(dom, "/*[local-name()='LogoutRequest']/*[local-name()='SessionIndex']/text()")[0].data;
-            console.log("log out hack")
+            //console.log("log out hack")
           let user = Meteor.users.findOne({ 'services.sso.session.sessionIndex':sessionIndex })
-            console.log(user)
+            //console.log(user)
           if(user){ //remove the session ID and the login token
             Meteor.users.update({_id:user._id},{ $set: {'services.sso.session': {}, 'services.resume.loginTokens' : [] } })
           }
-          console.log(user)
+            //console.log(user)
           res.writeHead(302, {'Location': Meteor.absoluteUrl('login')});
           res.end()
+          //----------- Hack end
           /* 
           //The code below should run instead of the hack above!!!
           Accounts.samlStrategy._saml.validatePostRequest(req.body, function(err, result){
@@ -199,51 +197,68 @@ if(settings.SSO_enabled && settings.SSO_emailIdentifier && settings.SSO_entrypoi
       Fiber(function() {
         try {     
           if (req.method === 'GET') {
-            // send the metadata
+            // send the metadata if metadata is in the path
             if (url.parse(req.url).pathname === '/metadata' || url.parse(req.url).pathname === '/metadata.xml') {
               res.writeHead(200, {'Content-Type': 'application/xml'});
               const decryptionCert = Assets.getText('cert.cert');
               res.end(Accounts.samlStrategy._saml.generateServiceProviderMetadata(decryptionCert), 'utf-8');
             } else {
-              // redirect to IdP (SP -> IdP)
+              // Otherwise redirect to IdP for login (SP -> IdP) (IDP responds with a POST handled below)
+              // This route gets called by the login popup window - otherwise, the RelayState will not be set.
               Accounts.samlStrategy._saml.getAuthorizeUrl(req, function (err, result) {
                 res.writeHead(302, {'Location': result});
                 res.end();
               });
             }
           }
-          // callback from IdP (IdP -> SP) to either logout or login
+          // POST callback from IdP (IdP -> SP) to either logout or login
           else if (req.method === 'POST') { 
             if (url.parse(req.url).pathname === '/logout') {
+              //----------- Hack start
+              //A hack to bypass the SSO stuff and log the user out using the SessionIndex in the IDP POST request
+              //(needed because passport-saml cannot validate the encrypted response)
+              //WARNING: This does not check that the POST came from the IDP
+              let xml = new Buffer(req.body.SAMLRequest, 'base64').toString('utf8');
+              let dom = new xmldom.DOMParser().parseFromString(xml); 
+              let sessionIndex = xpath(dom, "/*[local-name()='LogoutRequest']/*[local-name()='SessionIndex']/text()")[0].data;
+                //console.log("log out hack")
+              let user = Meteor.users.findOne({ 'services.sso.session.sessionIndex':sessionIndex })
+                //console.log(user)
+              if(user){ //remove the session ID and the login token
+                Meteor.users.update({_id:user._id},{ $set: {'services.sso.session': {}, 'services.resume.loginTokens' : [] } })
+              }
+                //console.log(user)
+              res.writeHead(302, {'Location': Meteor.absoluteUrl('login')});//this does not work, probably need a different response
+              res.end()
+              //----------- Hack end
+              //The code below should be used instead of the hack above!!! It should work if we disable encryption
+              //by not setting decryptionPvk in the SAML strategy. However, decryptionPvk is required to generate the
+              //metadata, so we would have to manually edit the metadata  
+              /* 
               Accounts.samlStrategy._saml.validatePostRequest(req.body, function(err, result){
                 if(!err){ //based on https://github.com/lucidprogrammer/meteor-saml-sp/blob/master/src/server/samlServerHandler.js
+                  console.log("validating post request")
                   console.log(result)
-                  let user = Meteor.users.findOne({ 'services.sso.sessions': {$elementMatch: {sessionIndex: result['sessionIndex']} }  }) 
-                  if(user){
-                    Meteor.users.update({_id:user._id},{ $pull: {'services.saml.sessions': {sessionIndex: result['sessionIndex']} },
-                                                         $set: {'services.resume.loginTokens' : []} } );  
-                    
+                  let user = Meteor.users.findOne({ 'services.sso.session.sessionIndex':result['sessionIndex'] }) 
+                  if(user){ //remove the session ID and the login token
+                    Meteor.users.update({_id:user._id},{ $set: {'services.sso.session': {}, 'services.resume.loginTokens' : [] } })
                   }
                   Accounts.samlStrategy._saml.getLogoutResponseUrl(req, function(err, logout){
                     if(error) throw new Error("Unable to generate logout response url");
                     res.writeHead(302, {'Location': logout});
                     res.end()
                   })
+                } else {
+                 console.log(err)  
+                 console.log(result)
                 }
-              })
+              }) */ // end of validate post request
                   
-            } else { //logging in
-                
-              //var xml = new Buffer(req.body.SAMLResponse, 'base64').toString('utf8');
-              //console.log(xml)  
-              //var doc = new xmldom.DOMParser().parseFromString(xml); 
-              //console.log(doc)
-                
-              ///////////////
+            } else {//POST request for login:
               Accounts.samlStrategy._saml.validatePostResponse(req.body, function (err, result) {
                 if (!err) {
-                  console.log(result) 
-                  email = result[settings.SSO_emailIdentifier] 
+                  //console.log(result) 
+                  email = result[settings.SSO_emailIdentifier] //settings.SSO_emailIdentifier has to be specified (see if at top)
                   firstname = settings.SSO_firstNameIdentifier ? result[settings.SSO_firstNameIdentifier] : 'Brice'
                   lastname = settings.SSO_lastNameIdentifier ? result[settings.SSO_lastNameIdentifier] : 'de Nice' 
                     
