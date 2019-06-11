6810af5f2625068007a9780e3f5b66c653e63d62
Ryan Martin <ryanmartinneutrino@gmail.com>
2019-06-11 14:27:40 -0400

    Update HistogramNumerical.jsx
    - histogram works nice, with nice auto bin width :-) should clean it up!

--- imports/ui/HistogramNumerical.jsx
+++ imports/ui/HistogramNumerical.jsx
@@ -35,7 +35,9 @@ class _HistogramNumerical extends Component {
   }
   render () {
     if (this.props.loading) return <div>Loading</div>
-
+    console.log(this.props.edges)
+    const xmin = this.props.edges[0]
+    const xmax = parseFloat((this.props.edges.slice(-1)[0]).toPrecision(5)) //gets the last element
     return (
       <div>
         <div className='ql-short-answer-list'>
@@ -48,8 +50,8 @@ class _HistogramNumerical extends Component {
           <BarChart className='ql-answer-distribution'
             height={190} width={500} data={this.props.data} barCategoryGap={0}
             margin={{top: 20, right: 10, left: -25, bottom: 5}}>
-            <XAxis dataKey='bin' />
-            <YAxis allowDecimals={false} />
+            <XAxis dataKey='bin'  type="number" interval='preserveStart' ticks={this.props.edges} domain={[xmin,xmax]} tickCount={this.props.edges.length} />
+            <YAxis allowDecimals={false} label='counts' />
             <Tooltip />
             <Bar dataKey="counts" fill="#30B0E7" />
           </BarChart>
@@ -61,16 +63,24 @@ class _HistogramNumerical extends Component {
 }
 
 export const HistogramNumerical = createContainer((props) => {
+
   const handle = Meteor.subscribe('responses.forQuestion', props.question._id)
   const question = props.question
   const attemptNumber = question.sessionOptions ? question.sessionOptions.attempts.length : 0
   // Get the responses for that attempt:
   const responses = Responses.find({ questionId: question._id, attempt: attemptNumber }, { sort: { createdAt: -1 } }).fetch()
+
+
   // Get the values into an array to be histogramed:
   //TODO swich to actual data!!
-  const values = _(responses).pluck('answer').map(Number)
-  //const values = [1,2,3,2,3,2,1,3,4,1,5,3,2,3,1,4,1,3,2,3,4,1]
-  //Histogram the data with a power of 10 as bin width
+  //const values = _(responses).pluck('answer').map(Number)
+  const values = [1,2,3,2,3,2,1,3,4,1,5,3,2,3,1,4,1,3,2,3,4,1]
+
+  //Upper limit on the number of bins in the hisogram based on entries:
+  //this one could be passed as a prop, as well as a rebinning prop..
+  const MAX_BINS = values.length < 200 ? 20 : 50
+  //Calculate the binning assuming just a root(n) number of bins, then
+  //clean things up to a "round" bin width, like 1, 0.5, 2
   let vmin = _(values).min()
   const vmax = _(values).max()
   const nvals = values.length
@@ -84,33 +94,53 @@ export const HistogramNumerical = createContainer((props) => {
   binWidth = Math.pow(10,power)
   //Now we also bring down the min value of the lowest bin to be a round number based on bin wdith
   vmin = Math.floor(vmin /binWidth)*binWidth
-  nbins = Math.round((vmax-vmin)/binWidth)+1
-  //Limit the number of bins to 20, increase the bin width
-  while(nbins > 20 ){
-    nbins = Math.floor(nbins/2)+1
-    binWidth *= 2
+  nbins = Math.floor((vmax-vmin)/binWidth)+1
+
+  //Finally, we lower the number of bins to be below MAX_BINS (if required)
+  //allow a tolerance of 20% on moxbins!
+  if(nbins > MAX_BINS) {
+    const nbrecall = nbins
+    let i = 0
+    let factor = 1
+    while(nbins > MAX_BINS){
+      factor = 5 * Math.pow(10,i)
+      nbins = nbrecall / factor
+      i++
+    }
+    //factor = 2 * Math.pow(10,i-1)
+    console.log(factor)
+    nbins = Math.floor(nbrecall/factor) + 1
+    binWidth *= factor
   }
+  //Add an extra bin at the end, so that its low edge could be the max value
+  nbins++
 
   console.log(vmin+" "+binWidth+" "+nbins)
   ///////////////////////////////////////////////
 
   //Count the values in each bin (+1 bin to get the upper value)
-  let counts = Array(nbins+1).fill(0)
+  let counts = nbins > 0 ? Array(nbins).fill(0) : Array(1).fill(0)
   values.forEach( function (val) {
     let index = Math.floor((val-vmin)/binWidth)
     counts[index]++
   })
   let data = []
-  for(let i=0;i<nbins+1;i++){
-    data.push({bin:vmin+i*binWidth,counts:counts[i]})
+  let edges = [] //used to draw the x axis
+  for(let i=0;i<nbins;i++){
+    //need to strip round off error, like 0.10000000000000004
+    edges.push(parseFloat((vmin+i*binWidth).toPrecision(10)))
+    //for the data, we use the centre of the bin:
+    data.push({bin:vmin+(i+0.5)*binWidth,counts:counts[i]})
   }
+  edges.push(vmin+nbins*binWidth)
   console.log(counts)
   console.log(data)
 
   return {
     responses: responses,
     loading: !handle.ready(),
-    data: data
+    data: data,
+    edges:edges
   }
 }, _HistogramNumerical)
 
