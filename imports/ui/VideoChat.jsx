2ddb229dbb5d23fb3e1f6d0b6799502065ca51d2
Ryan Martin <ryanmartinneutrino@gmail.com>
2020-07-02 16:00:35 -0400

    better skeleton

--- imports/ui/VideoChat.jsx
+++ imports/ui/VideoChat.jsx
@@ -38,30 +38,105 @@ export class _VideoChat extends Component {
   render () {
     if (this.props.loading) return <div className='ql-subs-loading'>Loading</div>
 
-/*
-    let options = {
-     roomName: 'qlicker-jitsi-1',
-     width: 500,
-     height:500,
-     interfaceConfigOverwrite: {
-      filmStripOnly: false,
-      SHOW_JITSI_WATERMARK: false,
-     },
-     configOverwrite: {
-      disableSimulcast: false,
-     },
-   }*/
-
     let domain = 'meet.qlicker.org'
 
     let vId = 123456543321
-
     const openChatWindow = () => { window.open('/course/'+this.props.courseId+'/videochatwindow/' +vId, 'Qlicker Video Chat', 'height=768,width=1024') }
+
+    /*
+    <div type='button' className='btn btn-secondary' onClick={openChatWindow}>
+      Open video chat window
+    </div>
+    */
+
+    const groupCategories = this.props.course.groupCategories
+
+    const categoriesWithChatEnabled = groupCategories //TODO select only with those enabled, and those selected in drop down
+
+    //A component to display group category name and controls to enable the chat
+    const VideoSessionControl = ({name, category}) => {
+      if (name){
+        return(
+          <li>
+            <div className='ql-group-list-item-name'>
+              {name}
+            </div>
+            <div className='ql-group-list-item-controls'>
+              <div className='btn'>
+                Enable
+              </div>
+            </div>
+          </li>
+        )
+      }
+      if (category) {
+        return(
+          <li>
+            <div className='ql-group-list-item-name'>
+              {category.categoryName} ({category.groups.length} groups)
+            </div>
+            <div className='ql-group-list-item-controls'>
+              <div className='btn '>
+                Enable
+              </div>
+            </div>
+          </li>
+        )
+      }
+    }
+
     return (
       <div className='ql-video-page'>
-        <div type='button' className='btn btn-secondary' onClick={openChatWindow}>
-          Open video chat window
+
+        <div className='ql-video-page-controls'>
+          <div className='ql-card'>
+            <div className='ql-header-bar'>
+              <h4>Enable/Disable Chat Rooms</h4>
+            </div>
+
+            <div className='ql-card-content'>
+              <ul>
+                <VideoSessionControl name={'Entire class'} />
+                { groupCategories.map(cat => {
+                  return <VideoSessionControl category={cat} key={'vc'+cat.categoryCount+cat.categoryName} />
+                  })
+                }
+              </ul>
+            </div>
+          </div>
         </div>
+
+        <div className='ql-video-page-rooms'>
+          <div className='ql-card'>
+            <div className='ql-header-bar'>
+              <h4>Join chat room</h4>
+            </div>
+
+            <div className='ql-card-content'>
+
+              <div className='btn' onClick={openChatWindow}>
+                Entire class
+              </div>
+
+              { categoriesWithChatEnabled.map(cat => {
+                return(
+                  <div key={'vcc'+cat.categoryCount+cat.categoryName} >
+                    <div> {cat.categoryName} </div>
+                      { cat.groups.map(group => {
+                          return <div key={'vccg'+cat.categoryCount+cat.categoryName+'g'+group.groupNumber+group.groupName} className='btn' onClick={openChatWindow}> {group.groupName} </div>
+                        })
+                      }
+                  </div>
+                )
+
+                })
+              }
+
+            </div>
+          </div>
+
+        </div>
+
       </div>
     )
   }
@@ -73,7 +148,7 @@ export const VideoChat = withTracker((props) => {
   const handle = Meteor.subscribe('courses.single', props.courseId)
 
   const course = Courses.findOne(props.courseId)
-
+  console.log(course)
   return {
     course: course,
     loading: !handle.ready()
